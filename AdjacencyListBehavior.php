7ce80fd5de46a25a9cb3c3de5651de2502e78ac6
a-zernaev <a.zernaev@gmail.com>
2021-12-01 14:06:57 +0400

    Update AdjacencyListBehavior.php

--- AdjacencyListBehavior.php
+++ AdjacencyListBehavior.php
@@ -30,6 +30,15 @@ class AdjacencyListBehavior extends Behavior
     const OPERATION_INSERT_BEFORE   = 4;
     const OPERATION_INSERT_AFTER    = 5;
     const OPERATION_DELETE_ALL      = 6;
+    
+    
+    public $multiTree = false;
+    
+    
+    public $treeAttribute = 'tree';
+    
+    
+    public $idAttribute = 'id';
 
     /**
      * @var string
@@ -128,9 +137,13 @@ class AdjacencyListBehavior extends Behavior
     public function getParents($depth = null)
     {
         $tableName = $this->owner->tableName();
-        $ids = $this->getParentsIds($depth);
-        $query = $this->owner->find()
-            ->andWhere(["{$tableName}.[[" . $this->getPrimaryKey() . "]]" => $ids]);
+        $ids = $this->getParentsIds($tree, $depth);
+        $query = $this->owner->find();
+        if ($this->multiTree === true) {
+            $tree = $this->owner->getAttribute($this->treeAttribute);
+            $query->andWhere(["{$tableName}.[[" . $this->treeAttribute . "]]" => $tree]);
+        }
+        $query->andWhere(["{$tableName}.[[" . $this->idAttribute . "]]" => $ids]);
         $query->multiple = true;
         return $query;
     }
@@ -141,16 +154,16 @@ class AdjacencyListBehavior extends Behavior
      * @throws Exception
      */
     public function getParentsOrdered($depth = null)
-    {
+    {        
         if ($depth === null && $this->_parentsOrdered !== null) {
             return $this->_parentsOrdered;
         }
-        $parents = $this->getParents($depth)->all();
+        $parents = $this->getParents($depth)->all();       
         $ids = array_flip($this->getParentsIds());
-        $primaryKey = $this->getPrimaryKey();
-        usort($parents, function($a, $b) use ($ids, $primaryKey) {
-            $aIdx = $ids[$a->$primaryKey];
-            $bIdx = $ids[$b->$primaryKey];
+        $idAttribute = $this->idAttribute;        
+        usort($parents, function($a, $b) use ($ids, $idAttribute) {
+            $aIdx = $ids[$a->$idAttribute];
+            $bIdx = $ids[$b->$idAttribute];
             if ($aIdx == $bIdx) {
                 return 0;
             } else {
@@ -169,7 +182,11 @@ class AdjacencyListBehavior extends Behavior
      */
     public function getParent()
     {
-        return $this->owner->hasOne($this->owner->className(), [$this->getPrimaryKey() => $this->parentAttribute]);
+        if ($this->multiTree === true) 
+            $condition[$this->treeAttribute] = $this->owner->getAttribute($this->treeAttribute);
+
+        $condition[$this->idAttribute] = $this->parentAttribute;        
+        return $this->owner->hasOne($this->owner->className(), $condition);
     }
 
     /**
@@ -179,9 +196,14 @@ class AdjacencyListBehavior extends Behavior
     {
         $tableName = $this->owner->tableName();
         $id = $this->getParentsIds();
-        $id = $id ? $id[count($id) - 1] : $this->owner->primaryKey;
-        $query = $this->owner->find()
-            ->andWhere(["{$tableName}.[[" . $this->getPrimaryKey() . "]]" => $id]);
+        $id = $id ? $id[count($id) - 1] : $this->idAttribute;
+        
+        $query = $this->owner->find();
+        
+        if ($this->multiTree === true)   
+            $query->andWhere(["{$tableName}.[[" . $this->treeAttribute . "]]" => $this->owner->getAttribute($this->treeAttribute)]);        
+        
+        $query->andWhere(["{$tableName}.[[" . $this->idAttribute . "]]" => $id]);            
         $query->multiple = false;
         return $query;
     }
@@ -196,10 +218,12 @@ class AdjacencyListBehavior extends Behavior
         $tableName = $this->owner->tableName();
         $ids = $this->getDescendantsIds($depth, true);
         if ($andSelf) {
-            $ids[] = $this->owner->getPrimaryKey();
+            $ids[] = $this->owner->getAttribute($this->idAttribute);
         }
-        $query = $this->owner->find()
-            ->andWhere(["{$tableName}.[[" . $this->getPrimaryKey() . "]]" => $ids]);
+        $query = $this->owner->find();
+        if ($this->multiTree === true)
+            $query->andWhere(["{$tableName}.[[" . $this->treeAttribute . "]]" => $this->owner->getAttribute($this->treeAttribute)]);
+        $query->andWhere(["{$tableName}.[[" . $this->idAttribute . "]]" => $ids]);
         $query->multiple = true;
         return $query;
     }
@@ -217,10 +241,10 @@ class AdjacencyListBehavior extends Behavior
             $descendants = $this->getDescendants($depth)->all();
         }
         $ids = array_flip($this->getDescendantsIds($depth, true));
-        $primaryKey = $this->getPrimaryKey();
-        usort($descendants, function($a, $b) use ($ids, $primaryKey) {
-            $aIdx = $ids[$a->$primaryKey];
-            $bIdx = $ids[$b->$primaryKey];
+        $idAttribute = $this->idAttribute;
+        usort($descendants, function($a, $b) use ($ids, $idAttribute) {
+            $aIdx = $ids[$a->$idAttribute];
+            $bIdx = $ids[$b->$idAttribute];
             if ($aIdx == $bIdx) {
                 return 0;
             } else {
@@ -235,7 +259,11 @@ class AdjacencyListBehavior extends Behavior
      */
     public function getChildren()
     {
-        $result = $this->owner->hasMany($this->owner->className(), [$this->parentAttribute => $this->getPrimaryKey()]);
+        if ($this->multiTree === true) 
+            $condition[$this->treeAttribute] = $this->treeAttribute;
+            
+        $condition[$this->parentAttribute] = $this->idAttribute;
+        $result = $this->owner->hasMany($this->owner->className(), $condition);
         if ($this->sortable !== false) {
             $result->orderBy([$this->behavior->sortAttribute => SORT_ASC]);
         }
@@ -255,8 +283,8 @@ class AdjacencyListBehavior extends Behavior
                 $query
                     ->from($modelClass::tableName() . ' children')
                     ->orderBy(null);
-            }])
-            ->andWhere(["children.[[{$this->parentAttribute}]]" => null]);
+            }]);
+            $query->andWhere(["children.[[{$this->parentAttribute}]]" => null]);
         $query->multiple = true;
         return $query;
     }
@@ -271,14 +299,18 @@ class AdjacencyListBehavior extends Behavior
             throw new NotSupportedException('prev() not allow if not set sortable');
         }
         $tableName = $this->owner->tableName();
-        $query = $this->owner->find()
-            ->andWhere([
-                'and',
-                ["{$tableName}.[[{$this->parentAttribute}]]" => $this->owner->getAttribute($this->parentAttribute)],
-                ['<', "{$tableName}.[[{$this->behavior->sortAttribute}]]", $this->owner->getSortablePosition()],
-            ])
-            ->orderBy(["{$tableName}.[[{$this->behavior->sortAttribute}]]" => SORT_DESC])
-            ->limit(1);
+        $query = $this->owner->find();
+        
+        if ($this->multiTree === true) 
+            $query->andWhere(["{$tableName}.[[{$this->treeAttribute}]]" => $this->owner->getAttribute($this->treeAttribute)]);
+
+        $query->andWhere([
+            'and',
+            ["{$tableName}.[[{$this->parentAttribute}]]" => $this->owner->getAttribute($this->parentAttribute)],
+            ['<', "{$tableName}.[[{$this->behavior->sortAttribute}]]", $this->owner->getSortablePosition()],
+        ])
+        ->orderBy(["{$tableName}.[[{$this->behavior->sortAttribute}]]" => SORT_DESC])
+        ->limit(1);
         $query->multiple = false;
         return $query;
     }
@@ -293,14 +325,18 @@ class AdjacencyListBehavior extends Behavior
             throw new NotSupportedException('next() not allow if not set sortable');
         }
         $tableName = $this->owner->tableName();
-        $query = $this->owner->find()
-            ->andWhere([
-                'and',
-                ["{$tableName}.[[{$this->parentAttribute}]]" => $this->owner->getAttribute($this->parentAttribute)],
-                ['>', "{$tableName}.[[{$this->behavior->sortAttribute}]]", $this->owner->getSortablePosition()],
-            ])
-            ->orderBy(["{$tableName}.[[{$this->behavior->sortAttribute}]]" => SORT_ASC])
-            ->limit(1);
+        $query = $this->owner->find();
+        
+        if ($this->multiTree === true) 
+            $query->andWhere(["{$tableName}.[[{$this->treeAttribute}]]" => $this->owner->getAttribute($this->treeAttribute)]);
+            
+        $query->andWhere([
+            'and',
+            ["{$tableName}.[[{$this->parentAttribute}]]" => $this->owner->getAttribute($this->parentAttribute)],
+            ['>', "{$tableName}.[[{$this->behavior->sortAttribute}]]", $this->owner->getSortablePosition()],
+        ])
+        ->orderBy(["{$tableName}.[[{$this->behavior->sortAttribute}]]" => SORT_ASC])
+        ->limit(1);
         $query->multiple = false;
         return $query;
     }
@@ -312,10 +348,14 @@ class AdjacencyListBehavior extends Behavior
      */
     public function getParentsIds($depth = null, $cache = true)
     {
-        if ($cache && $this->_parentsIds !== null) {
+        if ($cache && $this->_parentsIds !== null) {            
             return $depth === null ? $this->_parentsIds : array_slice($this->_parentsIds, 0, $depth);
         }
-
+        
+        $tree = null;
+        if ($this->multiTree === true)
+            $tree = $this->owner->getAttribute($this->treeAttribute);
+            
         $parentId = $this->owner->getAttribute($this->parentAttribute);
         if ($parentId === null) {
             if ($cache) {
@@ -324,24 +364,38 @@ class AdjacencyListBehavior extends Behavior
             return [];
         }
         $result     = [(string)$parentId];
-        $tableName  = $this->owner->tableName();
-        $primaryKey = $this->getPrimaryKey();
+        $tableName  = $this->owner->tableName();        
         $depthCur   = 1;
         while ($parentId !== null && ($depth === null || $depthCur < $depth)) {
             $query = (new Query())
                 ->select(["lvl0.[[{$this->parentAttribute}]] AS lvl0"])
-                ->from("{$tableName} lvl0")
-                ->where(["lvl0.[[{$primaryKey}]]" => $parentId]);
+                ->from("{$tableName} lvl0");
+                if (!is_null($tree)) {                   
+                    $query->where([
+                        "lvl0.[[{$this->treeAttribute}]]" => $tree,
+                        "lvl0.[[{$this->idAttribute}]]" => $parentId
+                    ]);
+                } else {
+                    $query->where(["lvl0.[[{$this->idAttribute}]]" => $parentId]);
+                }
+                
             for ($i = 0; $i < $this->parentsJoinLevels && ($depth === null || $i + $depthCur + 1 < $depth); $i++) {
-                $j = $i + 1;
-                $query
-                    ->addSelect(["lvl{$j}.[[{$this->parentAttribute}]] as lvl{$j}"])
-                    ->leftJoin("{$tableName} lvl{$j}", "lvl{$j}.[[{$primaryKey}]] = lvl{$i}.[[{$this->parentAttribute}]]");
+                $j = $i + 1;                
+                $query->addSelect(["lvl{$j}.[[{$this->parentAttribute}]] as lvl{$j}"]);                
+                if ($this->multiTree === true) {
+                    $query->leftJoin("{$tableName} lvl{$j}", [
+                        'AND',
+                        "lvl{$j}.[[{$this->treeAttribute}]] = lvl{$i}.[[{$this->treeAttribute}]]",
+                        "lvl{$j}.[[{$this->idAttribute}]] = lvl{$i}.[[{$this->parentAttribute}]]"
+                    ]);                            
+                } else {
+                    $query->leftJoin("{$tableName} lvl{$j}", "lvl{$j}.[[{$this->idAttribute}]] = lvl{$i}.[[{$this->parentAttribute}]]");                         
+                }
             }
-            if ($parentIds = $query->one($this->owner->getDb())) {
+            if ($parentIds = $query->one($this->owner->getDb())) {               
                 foreach ($parentIds as $parentId) {
                     $depthCur++;
-                    if ($parentId === null) {
+                    if ($parentId === null || $parentId === '') {
                         break;
                     }
                     $result[] = $parentId;
@@ -353,6 +407,7 @@ class AdjacencyListBehavior extends Behavior
         if ($cache && $depth === null) {
             $this->_parentsIds = $result;
         }
+        
         return $result;
     }
 
@@ -368,19 +423,29 @@ class AdjacencyListBehavior extends Behavior
             $result = $depth === null ? $this->_childrenIds : array_slice($this->_childrenIds, 0, $depth);
             return $flat && !empty($result) ? call_user_func_array('array_merge', $result) : $result;
         }
-
+        
+        $tree = null;
+        if ($this->multiTree === true)
+            $tree = $this->owner->getAttribute($this->treeAttribute);
+            
         $result       = [];
         $tableName    = $this->owner->tableName();
-        $primaryKey   = $this->getPrimaryKey();
         $depthCur     = 0;
-        $lastLevelIds = [$this->owner->primaryKey];
+        $lastLevelIds = [$this->owner->getAttribute($this->idAttribute)];
         while (!empty($lastLevelIds) && ($depth === null || $depthCur < $depth)) {
             $levels = 1;
             $depthCur++;
             $query = (new Query())
-                ->select(["lvl0.[[{$primaryKey}]] AS lvl0"])
-                ->from("{$tableName} lvl0")
-                ->where(["lvl0.[[{$this->parentAttribute}]]" => $lastLevelIds]);
+                ->select(["lvl0.[[{$this->idAttribute}]] AS lvl0"])
+                ->from("{$tableName} lvl0");
+                if (!is_null($tree)) {
+                    $query->where([
+                        "lvl0.[[{$this->treeAttribute}]]" => $tree,
+                        "lvl0.[[{$this->parentAttribute}]]" => $lastLevelIds                       
+                    ]);
+                } else {
+                    $query->where(["lvl0.[[{$this->parentAttribute}]]" => $lastLevelIds]);
+                }
             if ($this->sortable !== false) {
                 $query->orderBy(["lvl0.[[{$this->behavior->sortAttribute}]]" => SORT_ASC]);
             }
@@ -388,13 +453,22 @@ class AdjacencyListBehavior extends Behavior
                 $depthCur++;
                 $levels++;
                 $j = $i + 1;
-                $query
-                    ->addSelect(["lvl{$j}.[[{$primaryKey}]] as lvl{$j}"])
-                    ->leftJoin("{$tableName} lvl{$j}", [
+                $query->addSelect(["lvl{$j}.[[{$this->idAttribute}]] as lvl{$j}"]);
+                if ($this->multiTree === true) {
+                    $query->leftJoin("{$tableName} lvl{$j}", [
                         'and',
-                        "lvl{$j}.[[{$this->parentAttribute}]] = lvl{$i}.[[{$primaryKey}]]",
-                        ['is not', "lvl{$i}.[[{$primaryKey}]]", null],
+                        "lvl{$j}.[[{$this->treeAttribute}]] = lvl{$i}.[[{$this->treeAttribute}]]",
+                        "lvl{$j}.[[{$this->parentAttribute}]] = lvl{$i}.[[{$this->idAttribute}]]",
+                        ['is not', "lvl{$i}.[[{$this->idAttribute}]]", null],
                     ]);
+                } else  {
+                    $query->leftJoin("{$tableName} lvl{$j}", [
+                        'and',
+                        "lvl{$j}.[[{$this->parentAttribute}]] = lvl{$i}.[[{$this->idAttribute}]]",
+                        ['is not', "lvl{$i}.[[{$this->idAttribute}]]", null],
+                    ]);
+                }
+               
                 if ($this->sortable !== false) {
                     $query->addOrderBy(["lvl{$j}.[[{$this->behavior->sortAttribute}]]" => SORT_ASC]);
                 }
@@ -442,7 +516,7 @@ class AdjacencyListBehavior extends Behavior
     public function populateTree($depth = null, $with = null)
     {
         /** @var ActiveRecord[]|static[] $nodes */
-        $depths = [$this->owner->getPrimaryKey() => 0];
+        $depths = [$this->owner->getAttribute($this->idAttribute) => 0];
         $data = $this->getDescendantsIds($depth);
         foreach ($data as $i => $ids) {
             foreach ($ids as $id) {
@@ -468,10 +542,10 @@ class AdjacencyListBehavior extends Behavior
 
         $nodes[] = $this->owner;
         foreach ($nodes as $node) {
-            $key = $node->getPrimaryKey();
+            $key = $node->getAttribute($this->idAttribute);
             if (isset($relates[$key])) {
                 $node->populateRelation('children', $relates[$key]);
-            } elseif ($depth === null || (isset($depths[$node->getPrimaryKey()]) && $depths[$node->getPrimaryKey()] < $depth)) {
+            } elseif ($depth === null || (isset($depths[$node->getAttribute($this->idAttribute)]) && $depths[$node->getAttribute($this->idAttribute)] < $depth)) {
                 $node->populateRelation('children', []);
             }
         }
@@ -484,7 +558,8 @@ class AdjacencyListBehavior extends Behavior
      */
     public function isRoot()
     {
-        return $this->owner->getAttribute($this->parentAttribute) === null;
+        $parent_id = $this->owner->getAttribute($this->parentAttribute);
+        return $parent_id === null || $parent_id === '';
     }
 
     /**
@@ -494,7 +569,7 @@ class AdjacencyListBehavior extends Behavior
     public function isChildOf($node)
     {
         $ids = $this->getParentsIds();
-        return in_array($node->getPrimaryKey(), $ids);
+        return in_array($node->getAttribute($this->idAttribute), $ids);
     }
 
     /**
@@ -508,9 +583,12 @@ class AdjacencyListBehavior extends Behavior
     /**
      * @return ActiveRecord
      */
-    public function makeRoot()
+    public function makeRoot($tree = null)
     {
         $this->operation = self::OPERATION_MAKE_ROOT;
+        if ($this->multiTree === true && !is_null($tree)) {
+            $this->owner->setAttribute($this->treeAttribute, $tree);
+        }
         return $this->owner;
     }
 
@@ -681,7 +759,7 @@ class AdjacencyListBehavior extends Behavior
         if ($this->operation !== static::OPERATION_DELETE_ALL) {
             $this->owner->updateAll(
                 [$this->parentAttribute => $this->owner->getAttribute($this->parentAttribute)],
-                [$this->parentAttribute => $this->owner->getPrimaryKey()]
+                [$this->parentAttribute => $this->owner->getAttribute($this->idAttribute)]
             );
         }
         $this->operation = null;
@@ -693,11 +771,7 @@ class AdjacencyListBehavior extends Behavior
      */
     protected function getPrimaryKey()
     {
-        $primaryKey = $this->owner->primaryKey();
-        if (!isset($primaryKey[0])) {
-            throw new Exception('"' . $this->owner->className() . '" must have a primary key.');
-        }
-        return $primaryKey[0];
+        return $this->idAttribute;
     }
 
     /**
@@ -730,7 +804,7 @@ class AdjacencyListBehavior extends Behavior
     protected function insertIntoInternal($append)
     {
         $this->checkNode(false);
-        $this->owner->setAttribute($this->parentAttribute, $this->node->getPrimaryKey());
+        $this->owner->setAttribute($this->parentAttribute, $this->node->getAttribute($this->idAttribute));
         if ($this->sortable !== false) {
             if ($append) {
                 $this->behavior->moveLast();
@@ -767,10 +841,11 @@ class AdjacencyListBehavior extends Behavior
             return false;
         }
         $ids = $this->getDescendantsIds(null, true);
-        $ids[] = $this->owner->primaryKey;
-        $result = $this->owner->deleteAll([$this->getPrimaryKey() => $ids]);
+        $ids[] = $this->owner->getAttribute($this->idAttribute);
+        $result = $this->owner->deleteAll([$this->idAttribute => $ids]);
         $this->owner->setOldAttributes(null);
         $this->owner->afterDelete();
         return $result;
     }
+    
 }
